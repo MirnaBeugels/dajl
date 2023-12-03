9d8647bef296334a7775298fc84460da1a660bb3
Mirna Beugels <pamiro26@gmail.com>
2023-12-03 14:13:56 +0100

    3 bewegende cirkels met GSAP

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,39 +1,147 @@
+'use client'
+
 import styles from './styles.module.css'
 import Link from "next/link"
+import React from 'react'
+import { useRef, useLayoutEffect } from 'react'
+import { gsap } from 'gsap'
 
 export default function Denkaanjou({ children }) {
+
+    const blueCircle = useRef();
+    const pinkCircle = useRef();
+    const purpleCircle = useRef();
+
+    useLayoutEffect(() => {
+        
+        console.log(blueCircle)
+        
+        var blueCircleTl = gsap.timeline({repeat: -1})
+        var pinkCircleTl = gsap.timeline({repeat: -1})
+        var purpleCircleTl = gsap.timeline({repeat: -1})
+        
+        blueCircleTl.to(blueCircle.current, {
+            rotation: 0, 
+            x: '100vw',
+            y: '100vh',
+            duration: 30,
+        })
+        
+        blueCircleTl.to(blueCircle.current, {
+            rotation: 0, 
+            x: '0vw',
+            y: '40vh',
+            duration: 30,
+        });
+        
+        blueCircleTl.to(blueCircle.current, {
+            rotation: 0, 
+            x: '100vw',
+            y: '100vh',
+            duration: 30,
+        });
+        
+        blueCircleTl.to(blueCircle.current, {
+            rotation: 0, 
+            x: '0vw',
+            y: '0vh',
+            duration: 30,
+        });
+
+        pinkCircleTl.to(pinkCircle.current, {
+            rotation: 0, 
+            x: '-100vw',
+            y: '-20vh',
+            duration: 30,
+        })
+
+        pinkCircleTl.to(pinkCircle.current, {
+            rotation: 0, 
+            x: '0vw',
+            y: '0vh',
+            duration: 30,
+        })
+
+        pinkCircleTl.to(pinkCircle.current, {
+            rotation: 0, 
+            x: '-100vw',
+            y: '50vh',
+            duration: 30,
+        })
+
+        pinkCircleTl.to(pinkCircle.current, {
+            rotation: 0, 
+            x: '0vw',
+            y: '0vh',
+            duration: 30,
+        })
+
+        purpleCircleTl.to(purpleCircle.current, {
+            rotation: 0, 
+            x: '100vw',
+            y: '-60vh',
+            duration: 30,
+        })
+
+        purpleCircleTl.to(purpleCircle.current, {
+            rotation: 0, 
+            x: '0vw',
+            y: '-100vh',
+            duration: 30,
+        })
+
+        purpleCircleTl.to(purpleCircle.current, {
+            rotation: 0, 
+            x: '100vw',
+            y: '-60vh',
+            duration: 30,
+        })
+
+        purpleCircleTl.to(purpleCircle.current, {
+            rotation: 0, 
+            x: '0vw',
+            y: '0vh',
+            duration: 30,
+        })
+        
+        return () => { 
+        // Cleanup code (optional)
+        }
+    }, []);
+
     return (
         <html lang="en">
         <body className={styles.body}>
         <section className={styles.background}>
         <svg xmlns="http://www.w3.org/2000/svg" width="100vw" height="100vh" fill="none">
             <g filter="url(#blue)">
-                <circle cx="0vw" cy="0vh" r="125" fill="#106285"/>
+                <circle cx="0vw" cy="0vh" r="125" fill="#106285" ref={blueCircle}/>
             </g>
             <g filter="url(#pink)">
-                <circle cx="100vw" cy="50vh" r="125" fill="#891184"/>
+                <circle cx="100vw" cy="50vh" r="125" fill="#891184" ref={pinkCircle}/>
             </g>
             <g filter="url(#purple)">
-                <circle cx="0vw" cy="100vh" r="125" fill="#5B1189"/>
+                <circle cx="0vw" cy="100vh" r="125" fill="#5B1189" ref={purpleCircle}/>
             </g>
             <defs>
-                <filter id="blue" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <filter id="blue" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
                     <feGaussianBlur result="effect1_foregroundBlur_60_2259" stdDeviation="50"/>
                 </filter>
-                <filter id="pink" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <filter id="pink" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
                     <feGaussianBlur result="effect1_foregroundBlur_60_2259" stdDeviation="50"/>
                 </filter>
-                <filter id="purple"color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <filter id="purple"colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
                     <feGaussianBlur result="effect1_foregroundBlur_60_2259" stdDeviation="50"/>
                 </filter>
             </defs>
         </svg>
+        
         </section>
         <section className={styles.contentContainer}>
             <section className={styles.content}>
