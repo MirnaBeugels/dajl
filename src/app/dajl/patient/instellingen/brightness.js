32b8e0664ac742c5c0667c056905c42dcfa5a92c
Mirna Beugels <pamiro26@gmail.com>
2023-12-15 06:52:09 +0100

    send brightness en pause to db and mqtt

--- src/app/dajl/patient/instellingen/brightness.js
+++ src/app/dajl/patient/instellingen/brightness.js
@@ -1,30 +1,47 @@
 'use client'
 
-// Import needed styles, modules and components
-import styles from '../../../styles.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { faMinus } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react';
+// Import needed styles, modules, and components
+import styles from '../../../styles.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 
 const Brightness = () => {
+    const min = 0;
+    const max = 100;
 
-    const [brightness, setBrightness] = useState(false);
+    const [brightness, setBrightness] = useState(min);
 
-    const incrementBrightness = () => {
+    const clamp = (min, max) => (value) => (value <= min ? min : value >= max ? max : value);
+    const clampV = clamp(min, max);
+    const disableMin = brightness === min;
+    const disableMax = brightness === max;
 
-    }
+    const handleBrightnessChange = (value) => {
+        setBrightness(clampV(value));
+    };
 
-    return( <>
-
-        <div className={styles.dropdownMenuButtonContainer}>
-            <div className={styles.dropdownMenuUpDownButton}><FontAwesomeIcon icon={faMinus} onClick={incrementBrightness} /></div>
-            <div className={styles.dropdownMenuButtonCenter}>Helderheid: 100%</div>
-            <div className={styles.dropdownMenuUpDownButton}><FontAwesomeIcon icon={faPlus} onClick={incrementBrightness} /></div>
-        </div>
-        
+    return (
+        <>
+            <div className={styles.dropdownMenuButtonContainer}>
+                <div className={styles.dropdownMenuUpDownButton}>
+                    <FontAwesomeIcon
+                        icon={faMinus}
+                        onClick={() => handleBrightnessChange(brightness - 20)}
+                        disabled={disableMin}
+                    />
+                </div>
+                <div className={styles.dropdownMenuButtonCenter}>Helderheid: {brightness}%</div>
+                <div className={styles.dropdownMenuUpDownButton}>
+                    <FontAwesomeIcon
+                        icon={faPlus}
+                        onClick={() => handleBrightnessChange(brightness + 20)}
+                        disabled={disableMax}
+                    />
+                </div>
+            </div>
         </>
-    )
+    );
 };
 
 export default Brightness;
